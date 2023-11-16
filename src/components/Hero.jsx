9c3821b82cb0913a33c5c3c0895436adb0281b80
alanbacay02 <alan.social02@gmail.com>
2023-11-16 21:06:09 +0800

    Creates a component Hero.jsx

---
+++ src/components/Hero.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+
+const Hero = () => {
+  return (
+    <div className='w-full h-screen max-h-screen'>
+      <div className='w-full h-full'>
+        
+      </div>
+    </div>
+  )
+}
+
+export default Hero
