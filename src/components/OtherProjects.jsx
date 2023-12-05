e015135ee3c2f96ba44468430c63ec5d8b7c0d70
alanbacay02 <alan.social02@gmail.com>
2023-12-05 21:57:23 +0800

    Adds header to component

--- src/components/OtherProjects.jsx
+++ src/components/OtherProjects.jsx
@@ -2,7 +2,14 @@ import React from 'react'
 
 const OtherProjects = () => {
   return (
-    <div>OtherProjects</div>
+    <div className='w-full py-16'>
+      <div className='w-full max-w-[1200px] mx-auto'>
+        <div className='flex flex-col items-center justify-center w-full mb-14'>
+          <h2 className='font-bold min-w-fit text-4xl text-text mb-1'>Other Projects</h2>
+          <p className='text-lg'>Pure HTML/CSS Pages, Small React Apps, & More</p>
+        </div>
+      </div>
+    </div>
   )
 }
 
