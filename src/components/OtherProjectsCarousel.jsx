f592f4d29400a290ce65d29a78a746fb8d50a6c5
alanbacay02 <alan.social02@gmail.com>
2023-12-06 17:21:42 +0800

    Validates props for ProjectCard and adds error handling cases for languages props

--- src/components/OtherProjectsCarousel.jsx
+++ src/components/OtherProjectsCarousel.jsx
@@ -1,39 +1,94 @@
 import React from 'react'
+import PropTypes from 'prop-types'
 import useEmblaCarousel from 'embla-carousel-react'
-import { FaHtml5 } from "react-icons/fa";
-import { FaCss3Alt } from "react-icons/fa6";
-import { IoLogoJavascript } from "react-icons/io5";
+import { CssSvgIcon, HtmlSvgIcon, JavaScriptSvgIcon } from '../svg-components/IconSVG';
 
-const ProjectCard = () => {
+const PROJECT_CARD_INFO = [
+  {
+    projectTitle: 'Project 1',
+    languageIcons: [<HtmlSvgIcon />, <CssSvgIcon />],
+    projectDeployLink: '/#'
+  },
+  {
+    projectTitle: 'Project 2',
+    languageIcons: [<HtmlSvgIcon />, <JavaScriptSvgIcon />],
+    projectDeployLink: '/#'
+  },
+  {
+    projectTitle: 'Project 3',
+    languageIcons: [<HtmlSvgIcon />, <CssSvgIcon />],
+    projectDeployLink: '/#'
+  },
+  {
+    projectTitle: 'Project 4',
+    languageIcons: [<HtmlSvgIcon />, <CssSvgIcon />],
+    projectDeployLink: '/#'
+  },
+  {
+    projectTitle: 'Project 5',
+    languageIcons: [<HtmlSvgIcon />, <JavaScriptSvgIcon />],
+    projectDeployLink: '/#'
+  },
+  {
+    projectTitle: 'Project 6',
+    languageIcons: [<HtmlSvgIcon />, <CssSvgIcon />],
+    projectDeployLink: '/#'
+  }
+]
+
+const ProjectCard = ({ title, languages, deployLink }) => {
+  let LangCompArray = Array.isArray(languages) && languages.length > 0 ? [...languages] : [<HtmlSvgIcon />]
+
   return (
-    <div className='flex flex-col max-w-fit'>
-      <div className='h-[200px] w-[350px] bg-secondary'>
+    <div className='flex flex-col w-full max-w-[350px]'>
+      <div className='h-[200px] w-auto bg-secondary'>
         {/* Insert Image here */}
       </div>
       <div className='flex flex-col px-1 pt-2'>
         <div className='flex flex-row items-center justify-between'>
-          <h3 className='text-lg font-medium'>Project 1</h3>
-          <div className='flex flex-row justify-evenly items-center text-2xl'>
-            <FaHtml5 className='fill-orange-400' />
-            <FaCss3Alt className='fill-blue-400' />
-            <IoLogoJavascript className='fill-yellow-400' />
+          <h3 className='text-lg font-medium'>{title}</h3>
+          <div className='flex flex-row justify-evenly items-center'>
+              {LangCompArray.map((item, index) => {
+                return (
+                  <i key={index} className='h-[32px]'>{item}</i>
+                )
+              })}
           </div>
         </div>
-        <a href='/#' className='w-fit font-medium text-blue-500 hover:underline'>View Deployment &gt;</a>
+        <a href={deployLink} className='w-fit font-medium text-blue-500 hover:underline'>View Deployment &gt;</a>
       </div>
     </div>
   )
 }
+ProjectCard.defaultProps = {
+  title: 'Undefined Project',
+  languages: [],
+  deployLink: ''
+}
+
+ProjectCard.propTypes = {
+  title: PropTypes.string,
+  languages: PropTypes.array,
+  deployLink: PropTypes.string
+}
 
 const OtherProjectsCarousel = () => {
-  const [emblaRef] = useEmblaCarousel()
+  const [emblaRef] = useEmblaCarousel({loop: true, align: 'start', slidesToScroll: 3})
 
   return (
     <div className='embla overflow-hidden' ref={emblaRef}>
       <div className='embla__container flex'>
-        <div className='embla__project__slide'>
-          <ProjectCard />
-        </div>
+        {PROJECT_CARD_INFO.map((item, index) => {
+          return (
+            <div key={index} className='embla__project__slide'>
+              <ProjectCard
+                title={item.projectTitle}
+                languages={item.languageIcons}
+                deployLink={item.projectDeployLink}
+              />
+            </div>
+          )
+        })}
       </div>
     </div>
   )
