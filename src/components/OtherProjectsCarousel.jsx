43c8317cae78fc66b11aa7f2550550dcd8745128
alanbacay02 <alan.social02@gmail.com>
2023-12-06 23:19:38 +0800

    Changes component to utilize SwiperJS from embla-react-carousel

--- src/components/OtherProjectsCarousel.jsx
+++ src/components/OtherProjectsCarousel.jsx
@@ -1,7 +1,8 @@
-import React from 'react'
+import React, { useEffect, useRef } from 'react'
 import PropTypes from 'prop-types'
-import useEmblaCarousel from 'embla-carousel-react'
+import { BsChevronLeft, BsChevronRight } from 'react-icons/bs'
 import { CssSvgIcon, HtmlSvgIcon, JavaScriptSvgIcon } from '../svg-components/IconSVG';
+import { register } from 'swiper/element/bundle'
 
 const PROJECT_CARD_INFO = [
   {
@@ -40,11 +41,11 @@ const ProjectCard = ({ title, languages, deployLink }) => {
   let LangCompArray = Array.isArray(languages) && languages.length > 0 ? [...languages] : [<HtmlSvgIcon />]
 
   return (
-    <div className='flex flex-col w-full max-w-[350px]'>
-      <div className='h-[200px] w-auto bg-secondary'>
+    <div className='flex flex-col w-fit mx-auto'>
+      <div className='h-[200px] w-[330px] bg-secondary'>
         {/* Insert Image here */}
       </div>
-      <div className='flex flex-col px-1 pt-2'>
+      <div className='flex flex-col w-full px-1 pt-2'>
         <div className='flex flex-row items-center justify-between'>
           <h3 className='text-lg font-medium'>{title}</h3>
           <div className='flex flex-row justify-evenly items-center'>
@@ -73,22 +74,68 @@ ProjectCard.propTypes = {
 }
 
 const OtherProjectsCarousel = () => {
-  const [emblaRef] = useEmblaCarousel({loop: true, align: 'start', slidesToScroll: 3})
+  const swiperRef = useRef(null)
+
+  useEffect(() => {
+    // Register Swiper web component
+    register()
+    // Assigns current propert of `swiperRef` to `swiperContainer`
+    const swiperContainer = swiperRef.current
+    const params = {
+      breakpoints: {
+        0: {
+          slidesPerView: 1,
+          slidesPerGroup: 1
+        },
+        768: {
+          slidesPerView: 2,
+          slidesPerGroup: 2
+        },
+        1280: {
+          slidesPerView: 3,
+          slidesPerGroup: 3
+        }
+      },
+      loop: true
+    }
+
+    Object.assign(swiperContainer, params)
+    // initialize swiper
+    swiperContainer.initialize()
+  },[])
+
+  const handlePrevClick = () => {
+    const swiperInstance = swiperRef.current
+    swiperInstance.swiper.slidePrev()
+  }
+
+  const handleNextClick = () => {
+    const swiperInstance = swiperRef.current
+    swiperInstance.swiper.slideNext()
+  }
 
   return (
-    <div className='embla overflow-hidden' ref={emblaRef}>
-      <div className='embla__container flex'>
-        {PROJECT_CARD_INFO.map((item, index) => {
-          return (
-            <div key={index} className='embla__project__slide'>
-              <ProjectCard
-                title={item.projectTitle}
-                languages={item.languageIcons}
-                deployLink={item.projectDeployLink}
-              />
-            </div>
-          )
-        })}
+    <div className='flex flex-row'>
+      <div className='flex items-center justify-center text-6xl text-gray-400'>
+        <button onClick={() => {handlePrevClick()}}><BsChevronLeft /></button>
+      </div>
+      <div className='w-full overflow-hidden'>
+        <swiper-container ref={swiperRef} init='false'>
+          {PROJECT_CARD_INFO.map((item, index) => {
+            return (
+              <swiper-slide key={index}>
+                <ProjectCard
+                  title={item.projectTitle}
+                  languages={item.languageIcons}
+                  deployLink={item.projectDeployLink}
+                />
+              </swiper-slide>
+            )
+          })}
+        </swiper-container>
+      </div>
+      <div className='flex items-center justify-center text-6xl text-gray-400'>
+        <button onClick={() => {handleNextClick()}}><BsChevronRight /></button>
       </div>
     </div>
   )
