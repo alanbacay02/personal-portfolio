4392d994273b206bfc4891f826da2a5af1c8ab0b
alanbacay02 <alan.social02@gmail.com>
2023-12-13 00:39:55 +0800

    Updates components to be mobile responsive

--- src/components/OtherProjectsCarousel.jsx
+++ src/components/OtherProjectsCarousel.jsx
@@ -1,8 +1,9 @@
 import React, { useEffect, useRef } from 'react'
-import PropTypes from 'prop-types'
 import { BsChevronCompactLeft, BsChevronCompactRight } from 'react-icons/bs'
 import { BoostrapSvgIcon, CssSvgIcon, HtmlSvgIcon, JavaScriptSvgIcon, ReactSvgIcon, TailwindSvgIcon } from '../svg-components/IconSVG';
+import { motion } from 'framer-motion'
 import { register } from 'swiper/element/bundle'
+import OtherProjectCard from './OtherProjectCard';
 
 // Function to import all images from specified directory using Webpack's `require.context`
 const importAllImages = (r) => {
@@ -96,48 +97,6 @@ const PROJECT_CARD_INFO = [
   }
 ]
 
-// Creates a function component `ProjectCard` to create a card for each project
-const ProjectCard = ({ title, languages, deployLink, projectImg }) => {
-  // Spreads `languages` into `LangCompArray` if props `languages` is an array and is not empty, assigns <HtmlSvgIcon /> if else
-  let LangCompArray = Array.isArray(languages) && languages.length > 0 ? [...languages] : [<HtmlSvgIcon />]
-
-  return (
-    <div className='flex flex-col w-fit mx-auto'>
-      <a href={deployLink} target='_blank' rel='noreferrer' className='group hover:cursor-pointer'>
-        <div className='h-[215.2px] max-h-[215px] w-[330px] max-w-[330px] overflow-clip bg-secondary'>
-          <img src={projectImg} alt={title} className='object-cover object-center group-hover:scale-105 transition-all duration-300' />
-        </div>
-      </a>
-      <div className='flex flex-col w-full px-1 pt-2'>
-        <div className='flex flex-row items-center justify-between'>
-          <h3 className='text-lg font-medium'>{title}</h3>
-          <div className='flex flex-row justify-evenly items-center gap-1'>
-              {/* Maps over `LangCompArray` to return Svg Icon Components */}
-              {LangCompArray.map((item, index) => {
-                return (
-                  <i key={index} className='h-[32px]'>{item}</i>
-                )
-              })}
-          </div>
-        </div>
-        <a href={deployLink} target='_blank' rel='noreferrer' className='w-fit font-medium text-blue-500 hover:underline'>View Deployment &gt;</a>
-      </div>
-    </div>
-  )
-}
-// Defines default props for `ProjectCard` if props are undefined or empty
-ProjectCard.defaultProps = {
-  title: 'Undefined Project',
-  languages: [],
-  deployLink: ''
-}
-// Validates propTypes for `ProjectCard`
-ProjectCard.propTypes = {
-  title: PropTypes.string,
-  languages: PropTypes.array,
-  deployLink: PropTypes.string
-}
-
 const OtherProjectsCarousel = () => {
   // Creates a reference to the Swiper instance to interact with its methods and properties
   const swiperRef = useRef(null)
@@ -153,7 +112,7 @@ const OtherProjectsCarousel = () => {
           slidesPerView: 1,
           slidesPerGroup: 1
         },
-        768: {
+        648: {
           slidesPerView: 2,
           slidesPerGroup: 2
         },
@@ -187,8 +146,8 @@ const OtherProjectsCarousel = () => {
   }
 
   return (
-    <div className='flex flex-row'>
-      <div className='flex items-center justify-center text-6xl overflow-clip text-gray-400'>
+    <div className='flex flex-row max-w-[450px] sm:max-w-none mx-auto'>
+      <div className='hidden lg:flex items-center justify-center text-6xl overflow-clip text-gray-400'>
         <button
           onClick={() => {handlePrevClick()}}
           className='-mx-3 py-3 group'
@@ -201,18 +160,24 @@ const OtherProjectsCarousel = () => {
           {PROJECT_CARD_INFO.map((item, index) => {
             return (
               <swiper-slide key={index}>
-                <ProjectCard
-                  title={item.projectTitle}
-                  languages={item.languageIcons}
-                  deployLink={item.projectDeployLink}
-                  projectImg={item.projectImg}
-                />
+                <motion.div
+                  initial={{x:0}}
+                  whileInView={{x:2}}
+                  transition={{duration: 1}}
+                >
+                  <OtherProjectCard
+                    title={item.projectTitle}
+                    languages={item.languageIcons}
+                    deployLink={item.projectDeployLink}
+                    projectImg={item.projectImg}
+                  />
+                </motion.div>
               </swiper-slide>
             )
           })}
         </swiper-container>
       </div>
-      <div className='flex items-center justify-center text-6xl overflow-clip text-gray-400'>
+      <div className='hidden lg:flex items-center justify-center text-6xl overflow-clip text-gray-400'>
         <button
           onClick={() => {handleNextClick()}}
           className='-mx-3 py-3 group'
