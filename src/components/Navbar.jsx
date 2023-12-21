ddc1ba1d7e9363db2784a8fe5f18b8c80a78e627
alanbacay02 <alan.social02@gmail.com>
2023-12-21 16:27:22 +0800

    Creates a basic popout menu design. Creates state to track popout menu behavior

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -4,6 +4,7 @@ import { FaLinkedinIn } from "react-icons/fa";
 import { FaGithubAlt } from "react-icons/fa6";
 import { LuMoon, LuSunMoon } from "react-icons/lu";
 import { RxHamburgerMenu } from "react-icons/rx";
+import PopoutMenu from './PopoutMenu';
 
 const NAVBAR_ITEMS = [
   {
@@ -65,10 +66,14 @@ const Navbar = () => {
   return (
     <div className={`w-full h-fit px-4 text-text z-10 ${scrolling ? 'navbar-scroll bg-background py-4' : 'navbar-default py-4'}`}>
       <div className='w-full max-w-[1200px] mx-auto'>
+        {/* Parent Flex Container */}
         <div className='relative flex flex-row items-center justify-between'>
+          {/* Navbar Logo */}
           <div id='navbar-logo' className=' w-6 md:w-10 h-auto hover:cursor-pointer z-50'>
             <img src={CatCode} alt='cat_code_logo' />
           </div>
+
+          {/* Navbar List items on Medium and Larger devices */}
           <div className='hidden md:flex absolute w-full h-full items-center justify-center'>
             <ul className='flex flex-row items-center gap-10 z-50'>
               {NAVBAR_ITEMS.map((item, index) => {
@@ -82,7 +87,9 @@ const Navbar = () => {
             </ul>
           </div>
           
+          {/* Social Media Icons, Hamburger Menu and Dark Mode Button Container */}
           <div className='flex items-center gap-4 sm:gap-6 md:gap-8'>
+            {/* Social Media Icon Links */}
             <div className='hidden xs:flex flex-row gap-1 text-xl z-50'>
               {NAVBAR_ICONS.map((item, index) => {
                 return (
@@ -97,18 +104,24 @@ const Navbar = () => {
                 )
               })}
             </div>
+            {/* Dark Mode Button */}
             <button
               onClick={() => {setIsDarkMode(!isDarkMode)}}
               className='text-xl md:text-2xl hover:cursor-pointer z-50 hover:text-primary transition-all duration-300'
             >
               {isDarkMode ? <LuSunMoon /> : <LuMoon />}
             </button>
+
+            {/* Hamburger Menu */}
             <div className='md:hidden text-2xl md:text-3xl' onClick={handleMobileMenuClick}>
               <RxHamburgerMenu />
             </div>
           </div>
         </div>
       </div>
+
+      {/* Popout Menu */}
+      <PopoutMenu mobileMenu={mobileMenu} setMobileMenu={setMobileMenu} />
     </div>
   )
 }
--- src/components/PopoutMenu.jsx
+++ src/components/PopoutMenu.jsx
@@ -1,8 +1,115 @@
-import React from 'react'
+import React, { useEffect } from 'react'
+import { FaExternalLinkAlt, FaGithubSquare, FaLinkedin } from 'react-icons/fa';
+import { IoCloseOutline } from "react-icons/io5";
 
-const PopoutMenu = () => {
+const MENU_LIST_ITEMS = [
+  {
+    listItem: 'About',
+    href: ''
+  },
+  {
+    listItem: 'Skills',
+    href: ''
+  },
+  {
+    listItem: 'Projects',
+    href: ''
+  },
+  {
+    listItem: 'Contact',
+    href: ''
+  },
+]
+
+const PopoutMenu = ({ mobileMenu, setMobileMenu }) => {
+
+  useEffect(() => {
+    // Checks `mobileMenu` state to update scrolling behavior of 'body'
+    if (mobileMenu) {
+      document.body.classList.add('no-scroll') // Adds '.no-scroll' class to 'body' when `MobileMenu` is true
+    } else if (!mobileMenu) {
+      document.body.classList.remove('no-scroll') // Removes '.no-scroll' class from 'body' when `MobileMenu` is false
+    }
+
+    return () => {
+      // Removes '.no-scroll' class from 'body' when component unmounts
+      document.body.classList.remove('no-scroll')
+    }
+
+  }, [mobileMenu])
+
   return (
-    <div>PopoutMenu</div>
+    <div className={`w-[100%] max-w-[450px] h-screen fixed top-0 z-50 bg-background border-l border-secondary ${mobileMenu ? 'right-0' : '-right-full'} transition-all duration-300`}>
+      {/* Flex Parent Container */}
+      <div className='flex flex-col'>
+        {/* Menu Close Button */}
+        <button
+          onClick={() => { setMobileMenu(false) }}
+          className='text-2xl ml-auto m-4 mb-1'
+        >
+          <IoCloseOutline />
+        </button>
+
+        {/* Menu List Link Items */}
+        <ul className='flex flex-col text-lg p-4 py-5 gap-5'>
+          {MENU_LIST_ITEMS.map((item, index) => {
+            return (
+              <li key={index}>
+                <a href={item.href} className='w-fit'>{item.listItem}</a>
+              </li>
+            )
+          })}
+        </ul>
+
+        {/* Social Media Icons and Email Address Parent Container */}
+        <div className='flex flex-col gap-4 p-4 border-y border-gray-200'>
+          {/* Social Media Icons */}
+          <div>
+            <h3 className='text-base font-bold mb-2 text-gray-400'>Social Media</h3>
+            <div className='flex flex-row items-center gap-2 ml-1 text-2xl text-primary'>
+              <a
+                href='https://www.linkedin.com/in/alan-neale-bacay-ii-60aa48258/'
+                target='_blank'
+                rel='noreferrer'
+                className='hover:text-blue-600 transition-colors duration-300'>
+                  <FaLinkedin />
+              </a>
+              <a
+                href='https://github.com/alanbacay02'
+                target='_blank'
+                rel='noreferrer'
+                className='hover:text-blue-600 transition-colors duration-300'>
+                  <FaGithubSquare />
+              </a>
+            </div>
+          </div>
+
+          {/* Email Address */}
+          <div>
+            <h3 className='text-base font-bold mb-2 text-gray-400'>Email Address</h3>
+            <a
+              href='mailto:alanjbacay@gmail.com'
+              target='_blank'
+              rel='noreferrer'
+              className='ml-1 text-sm font-medium text-blue-500 hover:text-blue-600 transition-colors duration-300'
+            >
+              alanjbacay@gmail.com
+            </a>
+          </div>
+        </div>
+        
+        {/* Button Group Parent Container */}
+        <div className='flex flex-col gap-2 justify-center p-4 text-sm font-medium'>
+          {/* Resume Button */}
+          <button className='flex items-center gap-1.5 px-3 py-2 w-fit bg-secondary text-text rounded-lg'>
+            View Resume <FaExternalLinkAlt />
+          </button>
+
+          {/* Contact Button */}
+          <button className='px-3 py-2 w-fit bg-primary rounded-lg text-white'>Contact Me</button>
+        </div>
+      </div>
+    </div>
   )
 }
 
