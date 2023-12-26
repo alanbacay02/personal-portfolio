6456c5a4c145cec3694b9d81cd78af67dbce8deb
alanbacay02 <alan.social02@gmail.com>
2023-12-26 16:31:16 +0800

    Changes layout of component from flex container to grid

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -4,42 +4,46 @@ import { FaGithubAlt, FaLinkedin } from 'react-icons/fa'
 
 const Footer = () => {
   return (
-    <div className='w-full'>
-      <div className='w-full max-w-[1200px] h-fit mx-auto px-4 bg-text'>
-        <div className='grid grid-cols-3 py-4'>
-          <div className='col-span-2'>
-            <a href='' className='flex flex-row items-center gap-2'>
-                <div className='fill-white h-auto w-6'>
-                  <CatCodeLogoSvg />
-                </div>
-                <h2 className='font-bold text-white'>alanbacay</h2>
+    <div className='w-full bg-text'>
+      <div className='w-full max-w-[1200px] h-fit mx-auto px-4'>
+        <div className='grid sm:grid-cols-2 items-end py-4'>
+          {/* Logo and Social Div */}
+          <div className='flex flex-row sm:flex-col items-center sm:items-start justify-between'>
+            {/* Logo and Name */}
+            <a href='' className='w-fit flex flex-row items-center gap-2 sm:mb-4 sm:mt-1'>
+              <div className='fill-white h-auto w-6'>
+                <CatCodeLogoSvg />
+              </div>
+              <h2 className='font-bold text-lg text-white'>alanbacay</h2>
             </a>
-          </div>
-          <div className='col-span-1'>
-            <div className='flex flex-row justify-end'>
-              <a href='' className='text-2xl text-white'>
+            {/* Social Media Icons */}
+            <div className='flex flex-row gap-2'>
+              <a href='' className='text-2xl hover:text-primary transition-all duration-300 text-white'>
                 <FaLinkedin />
               </a>
-              <a href='' className='text-2xl text-white'>
+              <a href='' className='text-2xl hover:text-primary transition-all duration-300 text-white'>
                 <FaGithubAlt />
               </a>
             </div>
           </div>
-          <div className='col-span-3 flex flex-row justify-between text-white'>
+          {/* Contact Info */}
+          <div className='flex flex-col sm:flex-row justify-between sm:justify-evenly text-white'>
             <div>
-              <h4>Email</h4>
-              <a>alanjbacay@gmail.com</a>
+              <h4 className='font-bold text-xs text-gray-400'>Email</h4>
+              <a href='mailto:alanjbacay@gmail.com' className='text-sm hover:underline'>alanjbacay@gmail.com</a>
             </div>
             <div>
-              <h4>Phone</h4>
-              <p>+639064812232</p>
+              <h4 className='font-bold text-xs text-gray-400'>Phone</h4>
+              <p className='text-sm'>(+63)9064812232</p>
             </div>
           </div>
-          <div className='col-span-3'>
-            <div className='w-[95%] h-[1px] bg-gray-300' />
-            <div className='w-fit mx-auto flex flex-row gap-3 text-white'>
-              <p>alanbacay</p>
-              <a>Privacy Policy</a>
+          {/* Foot Note */}
+          <div className='col-span-full mt-5'>
+            <div className='w-[95%] h-[1px] bg-gray-600' />
+            <div className='w-fit mx-auto flex flex-row gap-2 text-xs mt-1 text-gray-400'>
+              <p>©alanbacay</p>
+              <p>•</p>
+              <a href='' className='underline hover:text-white'>Privacy Policy</a>
             </div>
           </div>
         </div>
