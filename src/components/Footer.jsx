25191e2f67e406642aa7976b6e986c941cc3214b
alanbacay02 <alan.social02@gmail.com>
2023-12-26 23:12:07 +0800

    Updates layout and icons of footer

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,6 +1,6 @@
 import React from 'react'
 import { CatCodeLogoSvg } from '../svg-components/IconSVG'
-import { FaGithubAlt, FaLinkedin } from 'react-icons/fa'
+import { FaGithubAlt, FaLinkedinIn } from 'react-icons/fa'
 
 const Footer = () => {
   return (
@@ -8,29 +8,24 @@ const Footer = () => {
       <div className='w-full max-w-[1200px] h-fit mx-auto px-4'>
         <div className='grid sm:grid-cols-2 items-end py-4'>
           {/* Logo and Social Div */}
-          <div className='flex flex-row sm:flex-col items-center sm:items-start justify-between'>
+          <div className='flex flex-row items-center gap-4'>
             {/* Logo and Name */}
-            <a href='' className='w-fit flex flex-row items-center gap-2 sm:mb-4 sm:mt-1'>
+            <a href='/#' className='w-fit flex flex-row items-center gap-2'>
               <div className='fill-white h-auto w-6'>
                 <CatCodeLogoSvg />
               </div>
               <h2 className='font-bold text-lg text-white'>alanbacay</h2>
             </a>
-            {/* Social Media Icons */}
-            <div className='flex flex-row gap-2'>
-              <a href='' className='text-2xl hover:text-primary transition-all duration-300 text-white'>
-                <FaLinkedin />
-              </a>
-              <a href='' className='text-2xl hover:text-primary transition-all duration-300 text-white'>
-                <FaGithubAlt />
-              </a>
-            </div>
           </div>
           {/* Contact Info */}
-          <div className='flex flex-col sm:flex-row justify-between sm:justify-evenly text-white'>
+          <div className='flex flex-col xs:flex-row justify-start sm:justify-end mt-4 sm:mt-0 gap-4 xs:gap-10 text-white'>
             <div>
               <h4 className='font-bold text-xs text-gray-400'>Email</h4>
-              <a href='mailto:alanjbacay@gmail.com' className='text-sm hover:underline'>alanjbacay@gmail.com</a>
+              <p className='text-sm hover:underline'>
+                <a href='mailto:alanjbacay@gmail.com'>
+                  alanjbacay@gmail.com
+                </a>
+              </p>
             </div>
             <div>
               <h4 className='font-bold text-xs text-gray-400'>Phone</h4>
@@ -38,12 +33,12 @@ const Footer = () => {
             </div>
           </div>
           {/* Foot Note */}
-          <div className='col-span-full mt-5'>
-            <div className='w-[95%] h-[1px] bg-gray-600' />
-            <div className='w-fit mx-auto flex flex-row gap-2 text-xs mt-1 text-gray-400'>
+          <div className='col-span-full flex flex-col items-end mt-5'>
+            <div className='w-full h-[0.5px] bg-gray-600' />
+            <div className='w-fit flex flex-row gap-2 text-xs mt-1 text-gray-400'>
               <p>©alanbacay</p>
               <p>•</p>
-              <a href='' className='underline hover:text-white'>Privacy Policy</a>
+              <a href='/#' className='underline hover:text-white'>Privacy Policy</a>
             </div>
           </div>
         </div>
