5b1ca39eeba2f68e80cd2a4ac0583ff3c0ae7218
alanbacay02 <alan.social02@gmail.com>
2023-12-29 19:30:15 +0800

    Creates a component ThemeContext to implement dark mode

---
+++ src/ThemeContext.js
@@ -0,0 +1,11 @@
+import React, { createContext } from 'react'
+
+const theme = createContext()
+
+const ThemeContext = () => {
+  return (
+    <div>ThemeContext</div>
+  )
+}
+
+export default ThemeContext
