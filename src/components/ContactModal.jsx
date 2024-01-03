be898e419e00cf8282040d3f5ffbc1ad815ea517
alanbacay02 <alan.social02@gmail.com>
2024-01-03 17:54:22 +0800

    Changes max width of contact model and changes social media icons

--- src/components/ContactModal.jsx
+++ src/components/ContactModal.jsx
@@ -1,5 +1,5 @@
 import React, { useEffect, useRef } from 'react'
-import { FaGithubSquare, FaLinkedin, FaLongArrowAltRight } from 'react-icons/fa'
+import { FaGithubAlt, FaLinkedinIn, FaLongArrowAltRight } from 'react-icons/fa'
 import headerImg from '../assets/polarProgrammer.png'
 import { IoCloseOutline } from "react-icons/io5";
 
@@ -40,14 +40,14 @@ const ContactModal = ({ showContactModal, setShowContactModal }) => {
   return (
     <div>
       <div className='fixed top-0 left-0 h-full w-full flex items-center justify-center px-4 z-30'>
-        <div ref={modalRef}  className='relative flex flex-col items-center w-full max-w-[400px] h-fit pb-4 bg-background dark:bg-gray dark:bg-zinc-800 rounded-lg'>
+        <div ref={modalRef}  className='relative flex flex-col items-center w-full max-w-[230px] md:max-w-[340px] h-fit pb-4 bg-background dark:bg-gray dark:bg-zinc-800 rounded-lg'>
           {/* Close Button at Top of Modal */}
           <div className='absolute top-0 right-0 z-50 text-2xl m-1 text-white dark:text-darkWhite'>
             <button onClick={() => {setShowContactModal(!showContactModal)}} className='hover:scale-125 transition-all duration-300'><IoCloseOutline /></button>
           </div>
 
           {/* SVG Header Img */}
-          <div className='w-full bg-sky-600 dark:bg-sky-700 mx-auto px-4 py-2 mb-4 rounded-t-lg'>
+          <div className='w-full bg-sky-600 dark:bg-sky-700 mx-auto px-8 py-2 mb-4 rounded-t-lg'>
             <img src={headerImg} alt='header_img.png' className='mx-auto' />
           </div>
 
@@ -59,14 +59,14 @@ const ContactModal = ({ showContactModal, setShowContactModal }) => {
 
           {/* Social Media Icons */}
           <div className='px-4'>
-              <div className='flex flex-row items-center text-2xl md:text-3xl gap-1 text-primary dark:text-darkPrimary'>
+              <div className='flex flex-row items-center text-xl sm:text-2xl gap-2 sm:gap-3 text-primary dark:text-darkPrimary'>
                 <a
                   href='https://www.linkedin.com/in/alanbacay/'
                   target='_blank'
                   rel='noreferrer'
                   onClick={() => { setShowContactModal(!showContactModal) }}
                   className='hover:text-blue-600 transition-colors duration-300'>
-                    <FaLinkedin />
+                    <FaLinkedinIn />
                 </a>
                 <a
                   href='https://github.com/alanbacay02'
@@ -74,7 +74,7 @@ const ContactModal = ({ showContactModal, setShowContactModal }) => {
                   rel='noreferrer'
                   onClick={() => { setShowContactModal(!showContactModal) }}
                   className='hover:text-blue-600 transition-colors duration-300'>
-                    <FaGithubSquare />
+                    <FaGithubAlt />
                 </a>
               </div>
           </div>
