ba01cdd89466ea1e67b774708dca0efaf1f352b6
alanbacay02 <alan.social02@gmail.com>
2024-01-03 14:49:25 +0800

    Adds image as header to contact modal. Finishes contact modal design.

--- src/components/ContactModal.jsx
+++ src/components/ContactModal.jsx
@@ -1,7 +1,7 @@
 import React, { useEffect, useRef } from 'react'
-import { FaGithubSquare, FaLinkedin, FaViber, FaWhatsapp } from 'react-icons/fa'
-
-const headerStyle = 'text-base font-bold mb-1 text-gray-400 dark:text-gray-400'
+import { FaGithubSquare, FaLinkedin, FaLongArrowAltRight } from 'react-icons/fa'
+import headerImg from '../assets/polarProgrammer.png'
+import { IoCloseOutline } from "react-icons/io5";
 
 const ContactModal = ({ showContactModal, setShowContactModal }) => {
   const modalRef = useRef()
@@ -26,9 +26,9 @@ const ContactModal = ({ showContactModal, setShowContactModal }) => {
       document.body.classList.remove('no-scroll')
       document.removeEventListener('mousedown', handleMouseDown)
     }
-  }, [showContactModal])
+  }, [showContactModal, setShowContactModal])
 
-  const handleEmailLinkClick = () => {
+  const handleScrollToContactClick = () => {
     setShowContactModal(!showContactModal)
 
     const contactElement = document.getElementById('contact')
@@ -39,28 +39,32 @@ const ContactModal = ({ showContactModal, setShowContactModal }) => {
 
   return (
     <div>
-      <div className='fixed top-0 left-0 h-full w-full flex items-center justify-center z-30'>
-        <div ref={modalRef}  className='flex flex-col p-6 max-w-[45%] h-fit bg-background dark:bg-gray dark:bg-zinc-800 rounded-lg'>
-          <div>
-            <h3 className='header'>Contact Me</h3>
-            <p className='paragraph'>
-              Feel free to contact me through any of the links below, or send me an email through the 
-              <button
-                onClick={handleEmailLinkClick}
-                className='text-primary dark:text-primary hover:underline ml-1'
-              >
-                email form
-              </button>!
-            </p>
+      <div className='fixed top-0 left-0 h-full w-full flex items-center justify-center px-4 z-30'>
+        <div ref={modalRef}  className='relative flex flex-col items-center w-full max-w-[400px] h-fit pb-4 bg-background dark:bg-gray dark:bg-zinc-800 rounded-lg'>
+          {/* Close Button at Top of Modal */}
+          <div className='absolute top-0 right-0 z-50 text-2xl m-1 text-white dark:text-darkWhite'>
+            <button onClick={() => {setShowContactModal(!showContactModal)}} className='hover:scale-125 transition-all duration-300'><IoCloseOutline /></button>
           </div>
-          <div className='w-full mt-2 mb-4 h-[1px] mx-auto bg-secondary dark:bg-gray-500' />
-          <div className='mb-4'>
-              <h4 className={headerStyle}>Socials</h4>
+
+          {/* SVG Header Img */}
+          <div className='w-full bg-sky-600 dark:bg-sky-700 mx-auto px-4 py-2 mb-4 rounded-t-lg'>
+            <img src={headerImg} alt='header_img.png' className='mx-auto' />
+          </div>
+
+          {/* Header and Paragraph */}
+          <div className='text-center mb-3 px-4'>
+            <h3 className='text-sm sm:text-base font-medium mb-3'>Open to Opportunities!</h3>
+            <p className='text-xs sm:text-sm'>Let's Chat! Reach out via the links below or drop a message using the  <button onClick={handleScrollToContactClick} className='pl-0.5 text-accent dark:text-darkPrimary hover:underline'>email form</button>. Looking forward to connecting!</p>
+          </div>
+
+          {/* Social Media Icons */}
+          <div className='px-4'>
               <div className='flex flex-row items-center text-2xl md:text-3xl gap-1 text-primary dark:text-darkPrimary'>
                 <a
                   href='https://www.linkedin.com/in/alanbacay/'
                   target='_blank'
                   rel='noreferrer'
+                  onClick={() => { setShowContactModal(!showContactModal) }}
                   className='hover:text-blue-600 transition-colors duration-300'>
                     <FaLinkedin />
                 </a>
@@ -68,56 +72,29 @@ const ContactModal = ({ showContactModal, setShowContactModal }) => {
                   href='https://github.com/alanbacay02'
                   target='_blank'
                   rel='noreferrer'
+                  onClick={() => { setShowContactModal(!showContactModal) }}
                   className='hover:text-blue-600 transition-colors duration-300'>
                     <FaGithubSquare />
                 </a>
               </div>
           </div>
-          <div className='mb-4'>
-            <h4 className={headerStyle}>Email</h4>
-            <div className='flex flex-row items-center text-sm sm:text-base gap-2 font-medium text-blue-500 dark:text-sky-400'>
-              <a
-                href='mailto:alanjbacay@gmail.com'
-                target='_blank'
-                rel='noreferrer'
-                className='hover:text-blue-600 transition-colors duration-300'
-              >
-                alanjbacay@gmail.com
-              </a>
-            </div>
-          </div>
-          <div>
-            <h4 className={headerStyle}>Phone</h4>
-            <div className='flex flex-col text-sm sm:text-base font-medium text-blue-500 dark:text-sky-400'>
-              <a
-                href='tel:+639491520850'
-                target='_blank'
-                rel='noreferrer'
-                className='w-fit hover:text-blue-600 transition-colors duration-300'
-              >
-                (+63) 949 152 0850
-              </a>
-              <p className='flex flex-row items-center text-[0.700rem] leading-normal text-zinc-500/80 dark:text-gray-200'>
-                Available on Whatsapp
-                <span className='pl-0.5 mr-1'><FaWhatsapp /></span>
-                and Viber
-                <span className='px-0.5'><FaViber /></span>
-              </p>
-            </div>
-          </div>
 
-          {/* Close button */}
+          {/* Divider Div */}
+          <div className='w-full h-[1.5px] bg-secondary dark:bg-gray-600 my-4' />
+
+          {/* View More Contacts Button */}
           <button
-            onClick={() => {setShowContactModal(!showContactModal)}}
-            className='w-fit ml-auto'
+            onClick={handleScrollToContactClick}
+            className='flex flex-row items-center gap-2 w-fit px-3 py-2 rounded-lg font-medium text-xs sm:text-sm bg-primary dark:bg-darkPrimary text-white dark:text-black hover:bg-blue-600 dark:hover:bg-blue-600 transition-colors duration-300'
           >
-            Close
+            View More
+            <FaLongArrowAltRight />
           </button>
         </div>
       </div>
 
       {/* Darkens Screen behind modal to give focus */}
-      <div className='fixed inset-0 bg-black/20 dark:bg-black/50 z-20' />
+      <div className='fixed inset-0 bg-black/50 z-20' />
     </div>
   )
 }
