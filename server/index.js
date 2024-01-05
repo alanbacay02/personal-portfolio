9f686c37adf17ceb0cec03abeaa78a4dfad39cc8
alanbacay02 <alan.social02@gmail.com>
2024-01-05 19:03:57 +0800

    Sets back CORS origin back to client origin

--- server/index.js
+++ server/index.js
@@ -3,7 +3,7 @@ const cors = require('cors')
 const app = express();
 
 const corsOptions = {
-  origin: '*', // Replace with your frontend domain
+  origin: 'https://www.alanbacay.dev', // Replace with your frontend domain
   methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',
   credentials: true, // If handling cookies or sessions
   optionsSuccessStatus: 200 // Some legacy browsers choke on 204
@@ -20,8 +20,9 @@ app.post('/submitForm', (req, res) => {
   const { name, email, message } = req.body;
   // Perform actions here to send email or handle the data
   const processedData = { name, email, message, serverMessage: 'Data processed on server!' };
-    console.log(processedData);
+  console.log(processedData); // Logs data to console
 
+  // Sends back a message and the data to client for testing
   res.status(200).json({
     message: 'Form submitted successfully.',
     processedData: processedData
