11bf434fe2370b992e2c571976a6d0fc7f6f2e3d
alanbacay02 <alan.social02@gmail.com>
2024-01-05 17:36:37 +0800

    Sets up EmailForm to make post request to server endpoint

--- client/src/components/EmailForm.jsx
+++ client/src/components/EmailForm.jsx
@@ -1,15 +1,40 @@
 import React, { useState } from 'react'
 import PrivacyPolicyModal from './PrivacyPolicyModal'
+import axios from 'axios'
 
 const formLabelStyle = 'text-sm font-medium px-2 mb-2 text-gray-400 dark:text-gray-300'
 const inputFieldStyle = 'text-sm border border-secondary dark:border-none rounded-md bg-background dark:bg-darkSecondary py-1 px-2 text-sm sm:text-base text-text dark:text-darkText focus:outline-none focus:shadow-xl'
 
 const EmailForm = () => {
   const [showPrivacyModal, setShowPrivacyModal] = useState(false)
+  // State used to track changes to name field
+  const [name, setName] = useState('')
+  // State used to track changes to email address field
+  const [emailAddress, setEmailAddress] = useState('')
+  // State used to track changes to message field
+  const [message, setMessage] = useState('')
+
+  const handleFormSubmit = async (e) => {
+    e.preventDefault()
+    try {
+      // Make a POST request to your Express server endpoint
+      const response = await axios.post('https://personal-portfolio-server-theta.vercel.app', {
+        name: name,
+        email: emailAddress,
+        message: message,
+      });
+
+      // Handle successful response (if needed)
+      console.log('Form submitted successfully!', response.data);
+    } catch (error) {
+      // Handle error if the request fails
+      console.error('Error submitting form:', error);
+    }
+  }
 
   return (
     <div className='w-full max-w-[400px] md:max-w-[550px]'>
-      <form className='flex flex-col justify-center'>
+      <form onSubmit={handleFormSubmit} className='flex flex-col justify-center'>
         {/* NAME FIELD */}
         <div className='flex flex-col mb-4'>
           <label
@@ -22,6 +47,8 @@ const EmailForm = () => {
             type='text'
             placeholder='Your name here'
             className={inputFieldStyle}
+            value={name}
+            onChange={(e) => {setName(e.target.value)}}
           />
         </div>
         {/* EMAIL FIELD */}
@@ -36,6 +63,8 @@ const EmailForm = () => {
             type='email'
             placeholder='Your email here'
             className={inputFieldStyle}
+            value={emailAddress}
+            onChange={(e) => {setEmailAddress(e.target.value)}}
             required
           />
         </div>
@@ -47,6 +76,8 @@ const EmailForm = () => {
             rows={10}
             placeholder='Enter message here...'
             className={`resize-none ${inputFieldStyle}`}
+            value={message}
+            onChange={(e) => {setMessage(e.target.value)}}
             required
           />
         </div>
@@ -67,10 +98,8 @@ const EmailForm = () => {
           <input
             id='submit_button'
             type='submit'
-            // className='font-medium text-lg text-white rounded-lg py-2 px-5 bg-primary hover:cursor-pointer hover:bg-blue-600 transition-colors duration-300'
-            className='font-medium text-sm button-style bg-blue-200 dark:bg-gray-600 text-gray-400 dark:text-gray-500'
+            className='font-medium text-lg text-white rounded-lg py-2 px-5 bg-primary hover:cursor-pointer hover:bg-blue-600 transition-colors duration-300'
             value='Send Email'
-            disabled
           />
         </div>
       </form>
