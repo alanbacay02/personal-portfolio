2be540fd0d3b99528f8a1e8050c9a16c82ecf8cc
alanbacay02 <alan.social02@gmail.com>
2024-01-05 20:49:26 +0800

    Sets up nodemailer to send email after handleFormSubmit is called

--- server/index.js
+++ server/index.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const cors = require('cors')
+const nodemailer = require('nodemailer')
 const app = express();
 
 const corsOptions = {
@@ -22,17 +23,43 @@ const password = process.env.EMAIL_PASSWORD
 console.log(password ? 'password is set from env' : 'password not set from env')
 
 // POST endpoint to handle form submissions
-app.post('/submitForm', (req, res) => {
+app.post('/submitForm', async (req, res) => {
   const { name, email, message } = req.body;
   // Gets data from client and stores it into processed data
   const processedData = { name, email, message, serverMessage: 'Data processed on server!' };
   console.log(processedData); // Logs data to console
 
-  // Sends back a message and the data to client for testing
-  res.status(200).json({
-    message: 'Form submitted successfully.',
-    processedData: processedData
-  });
+  // Nodemailer configuration
+  const transporter = nodemailer.createTransport({
+    service: 'gmail',
+    auth: {
+      user: process.env.EMAIL_ADDRESS,
+      pass: process.env.EMAIL_PASSWORD
+    },
+  })
+
+  // Email Content
+  const mailOptions =   {
+    from: email,
+    to: process.env.EMAIL_ADDRESS,
+    subject: 'Email sent from portfolio website',
+    text: `Hello,\n\nYou received a message from ${name} (${email}):\n${message}`
+  }
+
+  try {
+    // Send email
+    await transporter.sendMail(mailOptions)
+    console.log('Email sent successfully')
+
+    // Sending response back to the client
+    res.status(200).json({
+      message: 'Form submitted successfully.',
+      processedData: processedData,
+    });
+  } catch (error) {
+    console.error('Error sending email:', error)
+    res.status(500).json({ message: 'Error submitting form' });
+  }
 });
 
 app.use("/", (req, res) => {
