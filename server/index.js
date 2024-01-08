8177dd3d411f9996af2dedcc38f371d47e98f406
alanbacay02 <alan.social02@gmail.com>
2024-01-08 16:40:30 +0800

    Adds webhook endpoint

--- server/index.js
+++ server/index.js
@@ -11,6 +11,14 @@ const corsOptions = {
   allowedHeaders: 'Content-Type, Authorization',
 };
 
+const webhookOptions = {
+  origin: '*',
+  methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',
+  credentials: true, // If handling cookies or sessions
+  optionsSuccessStatus: 200, // Some legacy browsers choke on 204
+  allowedHeaders: 'Content-Type, Authorization',
+};
+
 // Sets CORS origin and content headers
 app.use(cors(corsOptions));
 
@@ -22,7 +30,7 @@ const auth_token = process.env.RESEND_API_TOKEN
 
 const resend = new Resend(auth_token)
 
-// POST endpoint to handle form submissions
+// submitForm endpoint to handle form submissions
 app.post('/submitForm', cors(corsOptions), async (req, res) => {
   const { name, email, message } = req.body;
   // Gets data from client and stores it into processed data
@@ -50,6 +58,13 @@ app.post('/submitForm', cors(corsOptions), async (req, res) => {
     res.status(500).json({ message: 'Error submitting form', error: error });
   }
 });
+
+// webhooks endpoint to handle webhook submissions
+app.post('/webhooks', cors(webhookOptions), async (req, res) => {
+  const payload = req.body
+  console.log(payload)
+  res.status(200)
+})
 
 app.use("/", (req, res) => {
   res.send("Server is running");
