7d5a827d1e13634e64fa45477c0115d5a05fbe9f
alanbacay02 <alan.social02@gmail.com>
2024-01-09 20:00:38 +0800

    Updates comments and variables to improve readability. Cleans up unecessary inline-css

--- client/src/components/Reveal.jsx
+++ client/src/components/Reveal.jsx
@@ -3,18 +3,29 @@ import PropTypes from 'prop-types'
 import { motion, useAnimation, useInView } from 'framer-motion'
 
 export const Reveal = ({ children }) => {
+  // Creates a reference to an element using `useRef()`
   const revealRef = useRef()
 
+  // Checks if referenced element is in viewport using `useInView()`
   const isInView = useInView(revealRef, { once: true })
 
+  // Initializing controls for main animation using `useAnimation()` hook
   const mainControls = useAnimation()
+
+  // Initializing controls for slide animation using `useAnimation()` hook
   const slideControls = useAnimation()
 
+  // Using `useEffect()` hook to trigger animations when reference animation is in viewport
   useEffect(() => {
+
+    // Checks if element is in view
     if (isInView) {
+      // Starts main animation when element is in view
       mainControls.start('visible')
+      // Starts slide animation when element is in view
       slideControls.start('visible')
     }
+    // Dependency array with variables that trigger the effect when changed
   }, [isInView, mainControls, slideControls])
 
   return (
@@ -45,26 +56,31 @@ export const Reveal = ({ children }) => {
 }
 // Validates propTypes for Reveal component
 Reveal.propTypes = {
-  children: PropTypes.element.isRequired // validates thats children is a jsx element
+  children: PropTypes.element.isRequired // validates that children is a jsx element
 }
 
 export const RevealNoSlider = ({ children }) => {
-  const revealRef = useRef()
+  // Creates a reference to an element using `useRef()`
+  const noSlideRevealRef = useRef()
 
-  const isInView = useInView(revealRef, { once: true })
+  // Checks if referenced element is in viewport using `useInView()`
+  const isInView = useInView(noSlideRevealRef, { once: true })
 
+  // Initializing controls for main animation using `useAnimation()` hook
   const mainControls = useAnimation()
-  const slideControls = useAnimation()
 
+  // Using `useEffect()` hook to trigger animations when reference animation is in viewport
   useEffect(() => {
+    // Checks if element is in view
     if (isInView) {
+      // Starts main animation when element is in view
       mainControls.start('visible')
-      slideControls.start('visible')
     }
-  }, [isInView, mainControls, slideControls])
+    // Dependency array with variables that trigger the effect when changed
+  }, [isInView, mainControls])
 
   return (
-    <div ref={revealRef} className='relative w-fit h-fit inline-block box-border overflow-hidden'>
+    <div ref={noSlideRevealRef} className='w-fit h-fit overflow-hidden'>
       <motion.div
         variants={{
           hidden: { opacity: 0, y: 100 },
@@ -76,20 +92,10 @@ export const RevealNoSlider = ({ children }) => {
       >
         {children}
       </motion.div>
-      {/* <motion.div
-        variants={{
-          hidden: { left: 0 },
-          visible: { left: '100%' }
-        }}
-        initial='hidden'
-        animate={slideControls}
-        transition={{ duration: 0.5, ease: 'easeIn'}}
-        className='absolute top-1 bottom-1 left-0 right-0 bg-primary dark:bg-darkPrimary z-20'
-      /> */}
     </div>
   )
 }
-// Validates propTypes for Reveal component
+// Validates propTypes for RevealNoSLider component
 RevealNoSlider.propTypes = {
-  children: PropTypes.element.isRequired // validates thats children is a jsx element
+  children: PropTypes.element.isRequired // validates that children is a jsx element
 }
